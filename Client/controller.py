5ae28c6a19035957e02378dcb47f14347d88a2c6
cfalz <cody.falzone@gmail.com>
2019-01-24 20:31:28 -0800

    Added a Create Listing form template

--- Client/controller.py
+++ Client/controller.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_bootstrap import Bootstrap
 from Client.requester import Requester
+from Client.static.Forms.AddListing import AddListingForm
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -12,7 +13,7 @@ def index(items_url="http://localhost:5001/items"):
     return render_template('index.html', items_url=items_url)
 
 @app.route('/items')
-def items():
+def get_items():
     items_list = client.get_items()
     number_of_cols = 4
     items_with_sublists = [items_list[i:i+number_of_cols] for i in range(0,len(items_list), number_of_cols)]
@@ -23,6 +24,13 @@ def items():
 def item(id):
     return render_template('item.html', id=id)
 
+@app.route('/add_listing')
+def add_listing():
+    data = {}
+    form = AddListingForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('add_listing.html', form=form, data=data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
