d2652141fb9cdddff48d7755e0e115534d905efe
cfalz <cody.falzone@gmail.com>
2019-04-13 11:06:17 -0700

    Add Contact Information Form

--- ClientReact/src/Presentational/Search/DropDown/index.js
+++ ClientReact/src/Presentational/Search/DropDown/index.js
@@ -2,21 +2,23 @@ import React from 'react'
 
 
 const DropDown = ({title, options, handleChange, handleSubmit}) =>
-    <div className={title}>
-        <h2> {title} </h2> <br />
-        {options.map((option) => console.log('id: ' + option.id + ' title: ' + option.title + ' content: ' + option.content))}
-            {options.map((option) =>
-                <div className={option.id}>
-                   <label> {option.title} </label>
-                   <br />
-                   <select defaultValue={option.selected_default} onChange={handleChange(option.title)}>
-                        {option.content.map((option) => <option value={option.value}> {option.value} </option>)}
-                   </select>
-                   <br />
-                </div>
-             )}
-         <input type="submit" value="Submit" />
-    </div>;
+    <form onSubmit={handleSubmit}>
+        <div className={title}>
+            <h2> {title} </h2> <br />
+            {options.map((option) => console.log('id: ' + option.id + ' title: ' + option.title + ' content: ' + option.content))}
+                {options.map((option) =>
+                    <div className={option.id}>
+                       <label> {option.title} </label>
+                       <br />
+                       <select defaultValue={option.selected_default} onChange={handleChange(option.title)}>
+                            {option.content.map((option) => <option value={option.value}> {option.value} </option>)}
+                       </select>
+                       <br />
+                    </div>
+                 )}
+             <input type="submit" value="Submit" />
+        </div>
+    </form>;
 
 export default DropDown;
 
